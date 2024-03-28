268baf28e2733c341e1e53626c58fe2bfeb9a437
webala <webd01@protonmail.com>
2024-03-28 13:33:45 +0300

    return config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-// import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+});
