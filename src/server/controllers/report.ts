c52d6af9ae96057a84072fb0ba9ceae644a39961
webala <webd01@protonmail.com>
2024-04-02 17:17:52 +0300

    change order of daily reports

--- src/server/controllers/report.ts
+++ src/server/controllers/report.ts
@@ -3,7 +3,7 @@ import DailyReport from "../models/report/index.js";
 
 export const getDailyReports = expressAsyncHandler(async (req, res) => {
     try {
-        const reports = await DailyReport.find()
+        const reports = await DailyReport.find().sort({createdAt: -1})
         res.json(reports).status(200)
     } catch (error) {
         res.status(500).json({message: "Unable to fetch reports"})
