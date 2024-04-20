b9b579df376f15cb4b294d12858eb3cd41203f35
webala <webd01@protonmail.com>
2024-04-20 12:19:54 +0300

    add sales lederboard

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,18 +1,29 @@
 import express from "express";
-import { deleteUser, editUserPassword, editUser, getAllUsers, handleRefreshToken, loginUser, logoutUser, registerUser, getUserProfile } from "../controllers/user.js";
+import {
+  deleteUser,
+  editUserPassword,
+  editUser,
+  getAllUsers,
+  handleRefreshToken,
+  loginUser,
+  logoutUser,
+  registerUser,
+  getUserProfile,
+  getSalesStats,
+} from "../controllers/user.js";
 import { isAuthorized } from "../middleware/index.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("", isAuthorized,getAllUsers)
-router.post("", registerUser)
-router.post("/login", loginUser)
-router.post("/refresh", handleRefreshToken)
-router.post("/logout", isAuthorized, logoutUser)
-router.put("/passwordchange/:id",isAuthorized, editUserPassword)
-router.put("/:id", isAuthorized, editUser)
+router.get("", isAuthorized, getAllUsers);
+router.post("", registerUser);
+router.post("/login", loginUser);
+router.post("/refresh", handleRefreshToken);
+router.post("/logout", isAuthorized, logoutUser);
+router.put("/passwordchange/:id", isAuthorized, editUserPassword);
+router.put("/:id", isAuthorized, editUser);
 router.get("/:id", isAuthorized, getUserProfile);
-router.delete("/:productId",isAuthorized, deleteUser)
-
+router.delete("/:productId", isAuthorized, deleteUser);
+router.get("/sales/leaderboard", isAuthorized, getSalesStats);
 
 export default router;
